09b5cf03f1e4dffce29c1dd707b0bf837be08766
miaoqi <miaoqi3@xdf.cn>
2023-12-28 00:54:05 +0800

    update blog

---
+++ atguigu-mybatis2022/MyBatis_Demo2/src/main/java/com/atguigu/mybatis/pojo/User.java
@@ -0,0 +1,90 @@
+package com.atguigu.mybatis.pojo;
+
+public class User {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Integer age;
+
+    private String sex;
+
+    private String email;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password, Integer age, String sex, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.sex = sex;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", age=").append(age);
+        sb.append(", sex='").append(sex).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
